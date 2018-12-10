be3041fd40911238833daae4af0eee20ae9ef6e5
chelllsea <44092576+chelllsea@users.noreply.github.com>
2018-12-10 12:46:12 -0500

    Add files via upload

---
+++ Lesson 7/problem2.py
@@ -0,0 +1,8 @@
+x = input ('Pick a number:')
+x = int(x)
+y = input ('Pick an other number:')
+y = int(y)
+
+total = x+y
+total = str(total)
+print('The answer is ' + total)
---
+++ Lesson 7/python3.py
@@ -0,0 +1,12 @@
+print('-' * 17)
+
+print('100th Birthday')
+
+age = input('What is your age today?')
+
+age = int(age)
+
+yearsleft = 100 - age
+
+year= yearsleft + 2018
+print(year)
